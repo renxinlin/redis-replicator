a90b8492a5600608db6f7e21353a4018a2defd4b
renxinlin <2284057845@qq.com>
2021-01-07 21:00:26 +0800

    滑动窗口阻塞队列和就绪队列处理正确性验证完毕

--- node/src/main/java/com/renxl/rotter/sel/LoadTask.java
+++ node/src/main/java/com/renxl/rotter/sel/LoadTask.java
@@ -5,7 +5,6 @@ import com.alibaba.dubbo.common.utils.NamedThreadFactory;
 import com.renxl.rotter.config.CompomentManager;
 import com.renxl.rotter.sel.window.buffer.WindowBuffer;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeSet;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -22,7 +21,6 @@ import java.util.concurrent.atomic.AtomicLong;
 public class LoadTask extends Task {
 
 
-
     private String targetRedis;
 
     /**
@@ -35,7 +33,6 @@ public class LoadTask extends Task {
      * 滑动窗口就绪队列 先进先出
      */
     private ArrayBlockingQueue<Long> currentReadySeqNum;
-
 
 
     private ExecutorService waitSeqProcessor;
@@ -51,12 +48,18 @@ public class LoadTask extends Task {
         currentReadySeqNum = new ArrayBlockingQueue(parallelism);
         this.setPipelineId(pipelineId);
         this.targetRedis = targetRedis;
+        /**
+         * 滑动窗口阻塞队列处理器
+         */
         waitSeqProcessor = new ThreadPoolExecutor(1, 1, 60, TimeUnit.SECONDS,
-                new ArrayBlockingQueue(0), new NamedThreadFactory("load-pipelineId-" + pipelineId),
+                new ArrayBlockingQueue(0), new NamedThreadFactory("load-window-wait-" + pipelineId),
                 new ThreadPoolExecutor.CallerRunsPolicy());
 
+        /**
+         * 滑动窗口就绪队列处理器
+         */
         readySeqProcessor = new ThreadPoolExecutor(1, 1, 60, TimeUnit.SECONDS,
-                new ArrayBlockingQueue(0), new NamedThreadFactory("load-pipelineId-" + pipelineId),
+                new ArrayBlockingQueue(0), new NamedThreadFactory("load-window-ready-" + pipelineId),
                 new ThreadPoolExecutor.CallerRunsPolicy());
 
     }
@@ -70,26 +73,31 @@ public class LoadTask extends Task {
 
     public void run() {
         // 滑动窗口处理
-        waitSeqProcessor.execute(()->{
-            WindowBuffer loadBuffer = CompomentManager.getInstance().getWindowManager().getLoadBuffer(getPipelineId());
-            long seqNumber = loadBuffer.get();
-            currentWaitSeqNum.add(seqNumber);
-            if (currentSeqNum.get() != seqNumber) {
+        waitSeqProcessor.execute(() -> {
+            while (true) {
+                WindowBuffer loadBuffer = CompomentManager.getInstance().getWindowManager().getLoadBuffer(getPipelineId());
+                long seqNumber = loadBuffer.get();
                 currentWaitSeqNum.add(seqNumber);
             }
         });
 
 
-        readySeqProcessor.execute(()->{
-            long seqNumber = currentWaitSeqNum.first();
-            if (currentSeqNum.get() != seqNumber) {
-                //
-                currentReadySeqNum.add(seqNumber);
-                // 滑动
-                currentSeqNum.addAndGet(1);
-            }else {
-                // 放回去 等待正确的数据处理
-                currentWaitSeqNum.add(seqNumber);
+        readySeqProcessor.execute(() -> {
+            while (true) {
+                long seqNumber = currentWaitSeqNum.pollFirst();
+                if (currentSeqNum.get() == seqNumber) {
+                    // tcp就绪队列
+                    try {
+                        currentReadySeqNum.put(seqNumber);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    // 滑动
+                    currentSeqNum.addAndGet(1);
+                } else {
+                    // 放回去 等待正确的数据处理
+                    currentWaitSeqNum.add(seqNumber);
+                }
             }
         });
         // load 阶段单线程处理 pipeline发送 增加速度
@@ -110,15 +118,12 @@ public class LoadTask extends Task {
 
                 // 滑动窗口尾部推进
                 String selecterIp = CompomentManager.getInstance().getMetaManager().getPipelineTaskIps().get(getPipelineId()).getSelecterIp();
-                CompomentManager.getInstance().getWindowManager().singleSelect(getPipelineId(),selecterIp);
+                CompomentManager.getInstance().getWindowManager().singleSelect(getPipelineId(), selecterIp);
 
 
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
-
-
 
         }
 
